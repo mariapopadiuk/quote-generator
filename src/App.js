e95fd7b0579ac43d99ff4090e165df4becfef11f
mariapopadiuk <mpopadiuk13@gmail.com>
2023-05-25 17:50:12 +0100

    get rundom quote

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import { useState } from 'react';
 import './App.css';
-//import './index.css';
 
 function App() {
 const [quote, setQuote] = useState('');
@@ -8,11 +7,15 @@ const [quote, setQuote] = useState('');
 function getQuote() {
   fetch('https://type.fit/api/quotes')
   .then((res) => res.json())
-  .then((data) => console.log(data))
+  .then((data) => {
+    let randomNumber = Math.floor(Math.random() * data.length);
+    setQuote(data[randomNumber]);
+  })
 }
 
   return (
     <div className="App">
+      {quote.text}
       <button onClick={getQuote}>Quote</button>
     </div>
   );
