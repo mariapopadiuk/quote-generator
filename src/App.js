f3af53b16d144e61c35095bd365fcd70b8e6796b
mariapopadiuk <mpopadiuk13@gmail.com>
2023-05-26 18:02:12 +0100

    possibility share in twitter

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, useRef } from 'react';
 import './App.css';
 
 function App() {
 const [quote, setQuote] = useState('');
-
+const textRef = useRef();
+let colors = ['#ffff00', "#90ee90", '#ffa500', '#ff68ff', '#a9a9e7'];
+  
 function getQuote() {
   fetch('https://type.fit/api/quotes')
   .then((res) => res.json())
@@ -17,14 +19,25 @@ useEffect(() => {
   getQuote()
 }, []);
 
+  useEffect(() => {
+    textRef.current.style.color = colors[Math.floor(Math.random() * colors.length)];
+  }, [quote]);
+
   return (
     <div className="App">
       <div className='quote'>
-        <p>{quote.text}</p>
-        <p>{quote.author}</p>
+        <p ref={textRef}>{quote.text}</p>
+        <p>Author: {quote.author}</p>
         <div className='btnContainer'>
-          <button className='btn'>Get quote</button>
-          <a href="" className='btn'>Tweet</a>
+          <button className='btn' onClick={getQuote}>Get quote</button>
+          <a 
+            href={`https://twitter.com/intent/tweet?text=${quote.text}`} 
+          target="_blank"
+          rel="noopener noreferrer"
+          className='btn'
+          >
+            Tweet
+            </a>
         </div>
       </div>
     </div>
